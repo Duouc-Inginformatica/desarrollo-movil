09c482c03c317069b5615997c6452910abc1c02f
Alejandro Bovet <ale.j1310@outlook.com>
2022-08-26 18:28:21 -0400

    se crearon las clases de login y registro general

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { AlertController,NavController  } from '@ionic/angular';//constructor asociado al envio de info
+import { FormControl,FormGroup,Validators   } from '@angular/forms';//import que permite funcionamiento del formulario
+import { Router,NavigationExtras } from '@angular/router';//permite que funcione las f.de envio de info
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
   styleUrls: ['./login.page.scss'],
 })
-export class LoginPage implements OnInit {
+export class Datosusers {
+value = "passUsers"
+Usuario = new FormGroup({
+  user: new FormControl('', [Validators.required, Validators.minLength(4)]),
+  password: new FormControl('', [Validators.required, Validators.minLength(8)]),
+});
 
-  constructor() { }
+constructor(private navCtrl: NavController,private router: Router) { }
 
-  ngOnInit() {
-  }
+
 
 }
--- src/app/registro/registro.page.ts
+++ src/app/registro/registro.page.ts
@@ -1,15 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import { AlertController,NavController  } from '@ionic/angular';//constructor asociado al envio de info
+import { FormControl,FormGroup,Validators   } from '@angular/forms';//import que permite funcionamiento del formulario
+import { Router,NavigationExtras } from '@angular/router';//permite que funcione las f.de envio de info
+
 
 @Component({
   selector: 'app-registro',
   templateUrl: './registro.page.html',
   styleUrls: ['./registro.page.scss'],
 })
-export class RegistroPage implements OnInit {
 
+export class RegistroGeneral  {
+    
+    usuario = new FormGroup({
+      user: new FormControl('',[Validators.required, Validators.minLength(4),Validators.maxLength(8)]),
+      Nombre: new FormControl ('',[Validators.required, Validators.minLength(3), Validators.maxLength(10)]),
+      Apellido: new FormControl ('',[Validators.required, Validators.minLength(3), Validators.maxLength(10)]),
+      rut: new FormControl(''),
+      sede:new FormControl('',[Validators.maxLength(3)]),
+      pass: new FormControl('',[Validators.required, Validators.minLength(4),Validators.maxLength(4)]),
+    });
+  
   constructor() { }
 
-  ngOnInit() {
-  }
+ 
 
 }
