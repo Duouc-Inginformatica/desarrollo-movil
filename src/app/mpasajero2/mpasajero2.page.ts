e6fab95ec95bff064599c9a38a37f807110f4f43
Josep <jobautistac@outlook.com>
2022-10-16 23:16:09 -0300

    Create mpasajero2.page.ts

---
+++ src/app/mpasajero2/mpasajero2.page.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-mpasajero2',
+  templateUrl: './mpasajero2.page.html',
+  styleUrls: ['./mpasajero2.page.scss'],
+})
+export class Mpasajero2Page implements OnInit {
+
+  
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
